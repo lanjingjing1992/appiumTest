94116ca5b027e5ec2e6f7004e34af7b1b5bce8a4
lanjingjing <2364365304@qq.com>
2017-11-09 14:43:50 +0800

    导航页

---
+++ auto/browser.py
@@ -0,0 +1,72 @@
+from appium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+class FindElement(object):
+    def mystartBrowser(self):
+        self.desired_capabilities = {}
+        self.desired_capabilities['platformName'] = 'Android'
+        self.desired_capabilities['platformVersion'] = '6.0.1'
+        self.desired_capabilities['deviceName'] = '32d0d84a'
+        self.desired_capabilities['appPackage'] = 'com.qiezzi.eggplant'
+        self.desired_capabilities['appActivity'] = 'com.qiezzi.eggplant.base.WelcomeActivity'
+        self.driver = webdriver.Remote('http://localhost:4723/wd/hub', self.desired_capabilities)
+        self.driver.implicitly_wait(30)
+        #宽高：1080 1920
+
+        self.width=self.driver.get_window_size()['width']
+        self.height=self.driver.get_window_size()['height']
+
+
+    def myfindId(self,id):
+        try:
+            ID=(By.ID,id)
+            WebDriverWait(self.driver,30,0.5).until(EC.presence_of_element_located(ID))
+            return self.driver.find_element_by_id(id)
+        except:
+            pass
+    def myfindName(self,name):
+        try:
+            NAME=(By.NAME,name)
+            WebDriverWait(self.driver,30,0.5).until(EC.presence_of_element_located(NAME))
+            return self.driver.find_element_by_name(name)
+        except:
+            pass
+    def myfindXpath(self,xpath):
+        try:
+            XPATH=(By.XPATH,xpath)
+            WebDriverWait(self.driver,30,0.5).until(EC.presence_of_element_located(XPATH))
+            return self.driver.find_element_by_xpath(xpath)
+        except:
+            pass
+
+    def mytimeSleep(self,number):
+        time.sleep(number)
+    def mycloseChrome(self):
+        self.driver.quit()
+
+    def myimplicitly_wait(self,number):
+        self.driver.implicitly_wait(number)
+    def myScroll(self,number,id):
+        ID=(By.ID,id)
+        WebDriverWait(self.driver,30,0.5).until(EC.presence_of_element_located(ID))
+
+
+        for i in range(number):
+            self.driver.implicitly_wait(30)
+            x1 = int(self.width * 0.75)
+            x2 = int(self.width * 0.05)
+            y = int(self.height * 0.5)
+            self.driver.swipe(x1, y, x2, y, 1000)
+
+
+
+
+
+
+
+
+
+
+
---
+++ auto/helloworld.py
@@ -0,0 +1,44 @@
+
+from auto.browser import FindElement
+import unittest
+class qiezi(unittest.TestCase):
+    def setUp(self):#导航页
+        self.myfind=FindElement()
+        self.myfind.mystartBrowser()
+
+    def testExperience(self):#立即体验
+        self.myfind.myScroll(4, 'com.qiezzi.eggplant:id/btn_firstinstall_intent')
+        self.myfind.myfindId('com.qiezzi.eggplant:id/btn_feel_right_now').click()
+
+    def testlogin(self):#登陆或者注册页面
+        myfind=self.myfind
+        try:
+
+            print('进入到登陆页面了')#已经登陆过了
+            myfind.myimplicitly_wait(20)
+            myfind.myfindId('com.qiezzi.eggplant:id/btn_main_my').click()#个人中心
+            myfind.myfindId('com.qiezzi.eggplant:id/tv_fragment_hospital_state').click()#未认证
+            myphton=myfind.myfindId('com.qiezzi.eggplant:id/tv_hospital_phone').click()
+            myName=myfind.myfindName\
+                ('com.qiezzi.eggplant:id/tv_hospital_contact').click()
+
+            print(myphton.text)#我的手机号
+            print(myName.text)#我的用户名
+        except:
+            print('从登陆页启动')#第一次登陆
+            myfind.myfindId('com.qiezzi.eggplant:id/edt_frist_login_accout').send_keys('15901337131')
+            myfind.myfindId('com.qiezzi.eggplant:id/edt_frist_login_password').send_keys('442131ljj')
+            myfind.myfindId('com.qiezzi.eggplant:id/btn_frist_login_immediately').click()#登陆
+    def tearDown(self):
+            pass
+
+
+
+#新页面--------------------------------------
+
+
+
+
+
+
+
